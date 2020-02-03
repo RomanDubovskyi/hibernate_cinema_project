26a0959a17e4b18d45a900b4d8e82e83fdceb4a0
Roman Dubovskyi <rdubowskij@gmail.com>
2020-02-03 15:17:45 +0200

    connected Hibernate to project implemented user model + dao layer for it

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,8 +1,25 @@
 package com.dev.cinema;
 
+import com.dev.cinema.annotations.Injector;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieService;
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    private static Injector injector = Injector.getInstance("com.dev.cinema");
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
 
-
+        try {
+            movieService.getAll().forEach(System.out::println);
+        } catch (DataProcessingException e) {
+            LOGGER.error("can't get all movies " + e);
+        }
     }
 }
---
+++ src/main/java/com/dev/cinema/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.dev.cinema.exceptions;
+
+public class DataProcessingException extends Exception {
+    public DataProcessingException(String massage) {
+        super(massage);
+    }
+}
