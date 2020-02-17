fea63a5bbc452ff202d76a0d30ff4d1187d44725
Roman Dubovskyi <rdubowskij@gmail.com>
2020-02-17 16:41:11 +0200

    final version without jsp (using rest controller)

--- src/main/java/com/dev/cinema/controllers/HelloController.java
+++ src/main/java/com/dev/cinema/controllers/HelloController.java
@@ -1,13 +1,12 @@
 package com.dev.cinema.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping(value = "/Hello", method = RequestMethod.GET)
+    @GetMapping(value = "/hello")
     public String sayHello() {
         return "Hello";
     }
